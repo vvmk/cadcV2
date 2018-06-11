369ca23ed0fcc24c93917425a4f917996ba2205b
vvmk <vincentmasiello@gmail.com>
2018-06-10 23:36:40 -0400

    header, background

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,15 +17,9 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'cxa'`, async(() => {
+  it(`should have as title 'complex aesthetic'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('cxa');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to cxa!');
+    expect(app.title).toEqual('complex aesthetic');
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cxa';
+  title = 'complex aesthetic';
+  subtitle = 'every time is party time';
 }
