1a1decec4b157bcb0740c455edbbdff8f49eb830
vvmk <vincentmasiello@gmail.com>
2018-06-10 23:43:52 -0400

    test all three browsers

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -7,6 +7,8 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
+      require('karma-firefox-launcher'),
+      require('karma-safari-launcher'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
@@ -25,7 +27,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'Firefox', 'Safari'],
     singleRun: false
   });
 };
